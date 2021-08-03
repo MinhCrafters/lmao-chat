ea5d050d31c4ccce7639e60df5b60f4088d20aea
Minh Le <levan@DESKTOP-57OGPA2.(none)>
2021-08-03 21:33:25 +0700

    make it better

--- public/js/app.js
+++ public/js/app.js
@@ -14,11 +14,22 @@ const messages = [];
 
 var socket = io();
 
-$(document).ready(function() {
-    setTimeout(function() {
+const wait = (delay = 0) =>
+    new Promise(resolve => setTimeout(resolve, delay));
+
+const setVisible = (elementOrSelector, visible) =>
+    (typeof elementOrSelector === 'string' ?
+        document.querySelector(elementOrSelector) :
+        elementOrSelector
+    ).style.display = visible ? 'block' : 'none';
+
+setVisible('.page', false);
+setVisible('#loading', true);
+
+document.addEventListener('DOMContentLoaded', () =>
+    wait(1000).then(() => {
         $("body").addClass("loaded");
-    }, 2000);
-});
+    }));
 
 socket.on('message', message => {
     if (message.type !== messageTypes.LOGIN) {
